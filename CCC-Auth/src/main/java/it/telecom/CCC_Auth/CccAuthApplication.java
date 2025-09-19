d2174fd37c5b58816f29f6418ff0bef98ce69150
siva <siva.ramisetty@capgemini.com>
2025-09-19 12:00:54 +0200

    1st commit

---
+++ CCC-Auth/src/main/java/it/telecom/CCC_Auth/CccAuthApplication.java
@@ -0,0 +1,13 @@
+package it.telecom.CCC_Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CccAuthApplication.class, args);
+	}
+
+}
---
+++ CCC-Auth/src/test/java/it/telecom/CCC_Auth/CccAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.telecom.CCC_Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
