420c65eeaf9e5d3f04b31db3de14d8e971496b1d
siva <siva.ramisetty@capgemini.com>
2025-09-28 12:23:48 +0200

    Basic validations

--- CCC-Auth/src/main/java/it/telecom/CCC_Auth/service/AuthService.java
+++ CCC-Auth/src/main/java/it/telecom/CCC_Auth/service/AuthService.java
@@ -15,6 +15,28 @@ public class AuthService {
 		
 		String db_password = "1234567";
 		
+		
+		if (loginData.getEmail() == null) {
+			
+			return "email should required";
+		}
+		
+		if (loginData.getEmail().length() == 0) {
+			
+			return "email should not be empty";
+		}
+		
+		 boolean isValid = (loginData.getEmail().matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")); 
+			
+			if (isValid != true) {
+				return "email format required"; 
+			}
+			
+			
+			
+		
+		
+		
 		if (db_email.equals(loginData.getEmail()) && db_password.equals(db_password)) {
 		
 		
